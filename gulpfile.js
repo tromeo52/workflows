e52edd237ea64ec554938b30b119520e09eef2c4
Tony Romeo <tromeo52@gmail.com>
2017-03-25 19:48:05 -0400

    Added CoffeeScript To Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-
-	gutil.log('Workflows are always task driven.');
-
+gulp.task('coffee', function(){
+	gulp.src('components/coffee/tagline.coffee')
+	.pipe(coffee({bare: true})
+	.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
 });
 
