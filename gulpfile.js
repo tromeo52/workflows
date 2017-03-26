baaf244822fb438eb16962319dfcfad4f2904dc6
Tony Romeo <tromeo52@gmail.com>
2017-03-25 21:57:53 -0400

    Added Browserify and JQuery

--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,28 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
-	'components/scripts/rclick.js',
-	'components/scripts/pixgrid.js',
-	'components/scripts/tagline.js',
-	'components/scripts/template.js'
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
 ];
 
-gulp.task('coffee', function(){
-	gulp.src(coffeeSources)
-	.pipe(coffee({bare: true})
-	.on('error', gutil.log))
-	.pipe(gulp.dest('components/scripts'))
+gulp.task('coffee', function () {
+     gulp.src(coffeeSources)
+     .pipe(coffee({bare: true})
+     .on('error', gutil.log))
+     .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 });
 
